d124a75cad9f2662404479810093d1b3b8025a69
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2025-04-23 12:02:21 +0900

    画像追加

--- Title.cpp
+++ Title.cpp
@@ -6,8 +6,17 @@
 
 Title::Title()
 {
-	
+	UI_Img[0] = LoadGraph("Resource/image/score_img.png");
+	UI_Img[1] = LoadGraph("Resource/image/highscore_img.png");
+	UI_Img[2] = LoadGraph("Resource/image/plyerlife.png");
+	UI_Img[3] = LoadGraph("Resource/image/time_img.png");
+	UI_Img[4] = LoadGraph("Resource/image/bomb_img.png");
 
+	LifeImg = LoadGraph("Resource/image/life_img.png");
+
+	FireImg = LoadGraph("Resource/image/FireBall_img.png");
+	WaterImg = LoadGraph("Resource/image/WaterBall_img.png");
+	LeafImg = LoadGraph("Resource/image/leaf_img.png");
 }
 
 Title::~Title()
@@ -33,11 +42,27 @@ void Title::Draw() const
 {
 	//SetBackgroundColor(255, 255, 255);
 
-	SetFontSize(100);
+	/*SetFontSize(100);
 	DrawFormatString(350, 100, 0xffffff, "タイトル画面");
 
 	SetFontSize(50);
 	DrawFormatString(550, 300, 0xffffff, "スタート");
 	DrawFormatString(550, 400, 0xffffff, "ヘルプ");
-	DrawFormatString(550, 500, 0xffffff, "エンド");
+	DrawFormatString(550, 500, 0xffffff, "エンド");*/
+
+	DrawBox(800, 0, 1280, 700, 0xffffff, TRUE);
+
+	DrawGraph(850, 50, UI_Img[0], TRUE);
+	DrawGraph(850, 100, UI_Img[1], TRUE);
+	DrawGraph(800, 150, UI_Img[2], TRUE);
+	DrawGraph(850, 200, UI_Img[3], TRUE);
+	DrawGraph(850, 250, UI_Img[4], TRUE);
+
+	DrawGraph(1000, 170, LifeImg, TRUE);
+	DrawGraph(1050, 170, LifeImg, TRUE);
+	DrawGraph(1100, 170, LifeImg, TRUE);
+
+	DrawGraph(100, 150, FireImg, TRUE);
+	DrawGraph(200, 150, WaterImg, TRUE);
+	DrawGraph(300, 150, LeafImg, TRUE);
 }
--- Title.h
+++ Title.h
@@ -4,9 +4,22 @@
 class Title :public AbstractScene
 {
 private:
+	enum class TITLE_MENU {
+		START,
+		HELP,
+		END
+	};
+
+	TITLE_MENU Menu_Number1;
+	TITLE_MENU Menu_Number2;
 
 	int TitleBGM;
 	int TitleSE;
+	int UI_Img[6];
+	int LifeImg;
+	int FireImg;
+	int WaterImg;
+	int LeafImg;
 
 public:
 	Title();
